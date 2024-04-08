ad6eaac5718adcf590c85e6ab920f27412be03e0
Shreyan Gupta <64853271+RA1NCS@users.noreply.github.com>
2024-04-07 21:49:31 -0400

    Completed HomePage (Non-Functional)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="border-b-2 border-b-red-500 py-6">
             <div className="container mx-auto flex justify-between items-center">
             
-                <Link to = "/" className="text-3x1 font-bold tracking-tight text-red-500">
+                <Link to = "/" className="text-3xl font-bold tracking-tight text-red-500">
                     Deliverrr
                 </Link>
                 
