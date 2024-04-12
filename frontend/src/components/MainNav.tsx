f779e7f6ee761a7ee6b611ccf03b8436cf2aadbd
Shreyan Gupta <64853271+RA1NCS@users.noreply.github.com>
2024-04-12 16:35:19 -0400

    Homepage Update

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,11 +1,11 @@
-import { Button } from "./ui/button";
+import { Button } from './ui/button'
 
 const MainNav = () => {
-    return (
-        <Button variant="ghost" className="font-bold hover:text-red-500 hover:big-white">
-        Log In
-        </Button>
-    )
+	return (
+		<Button variant="ghost" className="font-bold hover:text-red-500 hover:big-white font-poppins">
+			Log In
+		</Button>
+	)
 }
 
 export default MainNav
